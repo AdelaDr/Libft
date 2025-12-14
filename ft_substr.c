e6b82dcf1f787e38a08e220103c341672707a34c
AdelaDr <drahotovaadela@seznam.cz>
2025-12-14 12:31:24 +0100

    start >= ft_strlen(s)

--- ft_substr.c
+++ ft_substr.c
@@ -13,7 +13,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
@@ -39,6 +39,46 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr[i] = '\0';
 	return (substr);
 }
+*/
+static char	*ft_empty_str(void)
+{
+	char	*str;
+
+	str = malloc(1);
+	if (!str)
+		return (NULL);
+	str[0] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	s_len = 0;
+	while (s[s_len])
+		s_len++;
+	if (start >= s_len)
+		return (ft_empty_str());
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
 /*
 int	main(void)
 {
