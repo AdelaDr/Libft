d7d14e2fed45d36522fce818c50e50f04a1e51ed
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2025-12-15 19:30:29 +0100

    norm

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:28:30 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/15 19:28:37 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
+void    ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*temp;
+        t_list  *temp;
 
-	if (!lst || !del)
-		return;
-
-	while (*lst)
-	{
-		temp = (*lst)->next;
-		del((*lst)->content);
-		free(*lst);
-		*lst = temp;
-	}
-	*lst = NULL;
+        if (!lst || !del)
+                return ;
+        while (*lst)
+        {
+                temp = (*lst)->next;
+                del((*lst)->content);
+                free(*lst);
+                *lst = temp;
+        }
+        *lst = NULL;
 }
