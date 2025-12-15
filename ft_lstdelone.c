2a184d1825a15fed66a353055888182df098dff8
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2025-12-15 19:32:18 +0100

    norm

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:31:14 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/15 19:31:17 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void    ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-    if (!lst || !del)
-        return;
-    del(lst->content);
-    free(lst);
+        if (!lst || !del)
+                return ;
+        del(lst->content);
+        free(lst);
 }
