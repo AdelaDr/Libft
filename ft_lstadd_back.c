7e9eb360667eb68be99012056cbd8404f6cc06a8
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2025-12-15 19:22:33 +0100

    norm

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,20 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:20:09 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/15 19:20:15 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void    ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list *last;
+        t_list  *last;
 
-    if (!lst || !new)
-        return;
-
-    if (*lst == NULL)
-    {
-        *lst = new;
-        return;
-    }
-
-    last = *lst;
-    while (last->next)
-        last = last->next;
-    last->next = new;
+        if (!lst || !new)
+                return ;
+        if (*lst == NULL)
+        {
+                *lst = new;
+                return ;
+        }
+        last = *lst;
+        while (last->next)
+                last = last->next;
+        last->next = new;
 }
