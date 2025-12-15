f62968b9919a0997fb8091290b1ef81a4d925fbb
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2025-12-15 19:36:01 +0100

    norm

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:35:09 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/15 19:35:12 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst)
+t_list  *ft_lstlast(t_list *lst)
 {
-    if (!lst)
-        return (NULL);
-    while(lst->next != NULL)
-        lst = lst->next;
-    return (lst);
+        if (!lst)
+                return (NULL);
+        while (lst->next != NULL)
+                lst = lst->next;
+        return (lst);
 }
