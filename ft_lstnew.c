7d747f48eaf04e45608314594d9b666f7bd953a4
AdelaDr <133956939+AdelaDr@users.noreply.github.com>
2025-12-15 19:39:33 +0100

    norm

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 19:38:36 by adrahoto          #+#    #+#             */
+/*   Updated: 2025/12/15 19:38:38 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list  *ft_lstnew(void *content)
 {
-	t_list	*new;
+        t_list  *new;
 
-	new = (t_list *)malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	return (new);
+        new = (t_list *)malloc(sizeof(t_list));
+        if (!new)
+                return (NULL);
+        new->content = content;
+        new->next = NULL;
+        return (new);
 }
